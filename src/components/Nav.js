aac46e54050ba4fec6e4645f139c65a5ca7672e0
Shriya1412 <126519874+Shriya1412@users.noreply.github.com>
2024-02-16 00:02:11 +1300

    Navigation bar completed

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 import Logo from "../assets/Logo.svg";
 
 const Nav = () => {
+
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    }
     return (
-        <nav>
-            <img src={Logo} alt="Navigation Bar Logo" />
-        <ul>
-            <li> <a href="/">Home</a></li>
-            <li> <a href="/about">About</a></li>
-            <li> <a href="/menu">Menu</a></li>
-            <li> <a href="/reservation">Reservation</a></li>
-            <li> <a href="/login">Login</a></li>
-        </ul>
+        <nav className={`navbar ${menuOpen ? "open": ""}`}>
+            <a href = '/' className="logo">
+                <img src={Logo} alt="Navigation Bar Logo" />
+            </a>
+
+            {/* mobile nav */}
+            <div className="menu-icon" onClick={toggleMenu}>
+                <div className='bar'></div>
+                <div className='bar'></div>
+                <div className='bar'></div>
+            </div>
+
+            {/* nav items */}
+                <ul className={`nav-links ${menuOpen ? "visible" :""}`}>
+                    <li> <a href="/">Home</a></li>
+                    <li> <a href="/about">About</a></li>
+                    <li> <a href="/menu">Menu</a></li>
+                    <li> <a href="/reservation">Reservation</a></li>
+                    <li> <a href="/login">Login</a></li>
+                </ul>
         </nav>
     );
 }
