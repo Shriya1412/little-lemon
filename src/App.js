077868facc84b4de15cc74cbbe247d131c5e1cdb
Shriya1412 <126519874+Shriya1412@users.noreply.github.com>
2024-02-16 00:40:59 +1300

    Created a Header file and added a router to include a link

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import './App.css';
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Footer from './components/Footer';
 import Main from './components/Main';
 import Nav from './components/Nav';
+import Header from './components/Header';
 
 function App() {
   return (
-    <>
-    <Nav></Nav>
+    <Router>
+      <Nav/>
+      <Header/>
     <Main></Main>
     <Footer></Footer>
-    </>
+    </Router>
   );
 }
 
